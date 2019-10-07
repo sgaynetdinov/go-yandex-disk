d86c32aa43c10155974484088ecfbe5ef72842ed
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-10-07 15:49:43 +0500

    Refactoring

--- resource.go
+++ resource.go
@@ -6,12 +6,12 @@ import (
 
 type Resource struct {
 	Type     string `json:"type"`
-	Path     string `json: "path"`
-	Name     string `json: "name"`
-	Created  string `json: "created"`
-	Modified string `json: "modified"`
-	Md5      string `json: "md5,omitetype"`
-	Sha256   string `json: "sha256,omitetype"`
+	Path     string `json:"path"`
+	Name     string `json:"name"`
+	Created  string `json:"created"`
+	Modified string `json:"modified"`
+	Md5      string `json:"md5,omitetype"`
+	Sha256   string `json:"sha256,omitetype"`
 }
 
 func (client *Client) Stat(path string) (resource *Resource, err error) {
