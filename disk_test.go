c09b859d4fc77dc28d4d26b67b79582fc6cfb7ca
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2020-10-24 16:57:50 +0500

    Add url for 'disk.DiskInfo()'

--- disk_test.go
+++ disk_test.go
@@ -1,7 +1,6 @@
 package yandexdisk
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,28 +17,12 @@ var diskJSON = []byte(`{
   }
 }`)
 
-func TestDisk(t *testing.T) {
-	var disk Disk
-
-	json.Unmarshal(diskJSON, &disk)
-
-	if disk.TrashSize != 4631577437 {
-		t.Error("Disk.TrashSize")
-	}
-
-	if disk.TotalSpace != 319975063552 {
-		t.Error("Disk.TotalSpace")
-	}
-
-	if disk.UsedSpace != 26157681270 {
-		t.Error("Disk.UsedSpace")
-	}
-}
-
 func TestDiskInfo(t *testing.T) {
+	var req *http.Request
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(diskJSON)
+		req = r
 	}))
 	defer ts.Close()
 
@@ -50,6 +33,14 @@ func TestDiskInfo(t *testing.T) {
 
 	if err != nil {
 		t.Error("Error is not nil")
+	}
+
+	if req.URL.Path != "/v1/disk" {
+		t.Error(req.URL.Path)
+	}
+
+	if req.URL.RawQuery != "" {
+		t.Error(req.URL.RawQuery)
 	}
 
 	if disk.TrashSize != 4631577437 {
@@ -66,19 +57,25 @@ func TestDiskInfo(t *testing.T) {
 }
 
 func TestDiskInfoIfStatusNot200(t *testing.T) {
+	var req *http.Request
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(`{
   			"description": "resource already exists",
   			"error": "PlatformResourceAlreadyExists"
 		}`))
+		req = r
 	}))
 	defer ts.Close()
 
 	client := NewClient("YOUR_TOKEN")
 	client.apiURL = ts.URL
 
-	_, err := client.DiskInfo()
+	got, err := client.DiskInfo()
+
+	if got != nil {
+		t.Error()
+	}
 
 	if err == nil {
 		t.Error("Error is nil")
@@ -88,4 +85,11 @@ func TestDiskInfoIfStatusNot200(t *testing.T) {
 		t.Error("Invalid error message")
 	}
 
+	if req.URL.Path != "/v1/disk" {
+		t.Error(req.URL.Path)
+	}
+
+	if req.URL.RawQuery != "" {
+		t.Error(req.URL.RawQuery)
+	}
 }
